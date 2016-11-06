8fdfabbddbde5fd671dce4b48ff48cd2d1777257
Ashwin Ramesh <Ashwin@wirelessprv-10-192-237-78.near.illinois.edu>
2016-11-05 20:08:13 -0500

    made changes to xmlfile

--- arv_map.php
+++ arv_map.php
@@ -40,12 +40,22 @@
           center: {lat: 22, lng: -70.109291},
         });
 
-	var state = 'Texas';
-	var path = 'resources/us_borders.xml';
-	var parser = new DOMParser();
-	var xmlDoc = parser.parseFromString(path,"text/xml"); 
+      function loadXMLDoc() {
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function() {
+                  if (this.readyState == 4 && this.status == 200) {
+                      putIntoDoc(this);
+                  }
+            };
+            xmlhttp.open("GET", "resources/us_borders.xml", true);
+            xmlhttp.send();
+      }
+      function myFunction(xml)
+      {
+         return xml.responseXML;
+      }
 
-	// xmlDoc.getElementsByTagName(state).childNodes[0].nodeValue;
+	var xmlDoc = loadXMLDoc();
 	var statesize = xmlDoc.getElementsByTagName("state")[0].childNodes.length;
 	var coords = [];
   var state = document.getElementsByTagName("state")[0];
